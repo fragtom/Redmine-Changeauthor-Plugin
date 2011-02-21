b4657b9f33c3621db1fe6f3eedfab1d6c1ba82be
Tom Stark <info@tstark.de>
2011-02-21 15:29:11 +0100

    #3 added log-behavior

--- redmine_changeauthor/init.rb
+++ redmine_changeauthor/init.rb
@@ -8,6 +8,8 @@ Redmine::Plugin.register :redmine_changeauthor do
   version '0.0.2' 
 
   permission :change_author, :require => :member
-    
+
+  settings :default => {'redmine_changeauthor_log_setting' => 'no'}, :partial => 'settings/redmine_changeauthor_settings'
+
 end
 
